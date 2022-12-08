21d6a1e0a3b5991d6e0c788a011b9ecc9dd9ed12
Benjamin Nesom <17NesomB@fulford.york.sch.uk>
2022-12-08 10:27:45 +0000

    Add project files.

---
+++ RegEx/ChocolateBar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegEx
+{
+    class ChocolateBar
+    {
+        public string Name;
+        public int Calories;
+        public int Price;
+        public int Weight;
+
+        public ChocolateBar(string name, int calories, int price, int weight)
+        {
+            Name = name;
+            Calories = calories;
+            Price = price;
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return ($"{Name}: {Weight}g {Calories} calories {Price}p");
+        }
+
+    }
+}
---
+++ RegEx/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string longestName = "";
+            int highestCals = 0;
+            int totalWeight = 0;
+            int cheapest = 1000;
+
+            
+
+            string path = "Data.txt";
+            using(StreamReader r = new StreamReader(path))
+            {
+                while(!r.EndOfStream)
+                {
+                   string input = r.ReadLine();
+
+                    Match m = Regex.Match(input, @"([A-Za-z]+[0-9]+) (\d+) (\d+) (\d+)");
+                    string name = m.Groups[1].Value;
+                    int calories = int.Parse(m.Groups[2].Value);
+                    int price = int.Parse(m.Groups[3].Value);
+                    int weight = int.Parse(m.Groups[4].Value);
+
+                    if (name.Length > longestName.Length)
+                    {
+                        longestName = name;
+                    }
+
+                    if (calories > highestCals)
+                    {
+                        highestCals = calories;
+                    }
+
+                    totalWeight += weight;
+
+                    if (price < cheapest)
+                    {
+                        cheapest = price;
+                    }
+
+
+
+
+                }
+
+                Console.WriteLine(longestName);
+                Console.WriteLine(highestCals);
+                Console.WriteLine(totalWeight);
+                Console.WriteLine(cheapest);
+
+                
+
+
+            }
+
+            
+        }
+    }
+}
